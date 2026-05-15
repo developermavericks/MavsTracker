00b334cf7268a92534242aa8f7ef668e50b045fb
satyam1289 <ss1084169@gmail.com>
2026-05-15 16:25:50 +0530

    chore: update satyam.singh to manager role for testing

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -44,7 +44,6 @@ export const authenticate = async (req: AuthRequest, res: Response, Next: NextFu
       'smriti@themavericksindia.com',
       'gaurav@themavericksindia.com',
       'avinash@themavericksindia.com',
-      'satyam.singh@themavericksindia.com',
       'arunkumar@themavericksindia.com',
       'divyanshsharma@themavericksindia.com',
       'developerteam@themavericksindia.com'
@@ -66,7 +65,8 @@ export const authenticate = async (req: AuthRequest, res: Response, Next: NextFu
       'anil@themavericksindia.com',
       'viviqa@themavericksindia.com',
       'ananya@themavericksindia.com',
-      'kavita@themavericksindia.com'
+      'kavita@themavericksindia.com',
+      'satyam.singh@themavericksindia.com'
     ];
 
     let assignedRole = 'team';
